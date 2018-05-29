85bcc047bca2c0318985861fd4eb944977d90086
Da wae <eric.volmer@hotmail.com>
2018-05-29 11:34:32 +0200

    s

---
+++ JavaApp/VIAFit/src/MainPackage/Event.java
@@ -0,0 +1,20 @@
+package MainPackage;
+
+public class Event
+{
+	private String className;
+	private boolean requiresQualification;
+	private int maxMembers;
+
+	public Event(String className, MyDate startDate, MyDate endDate, MyClock startTIme, MyClock endTime)
+	{
+		this.className=className;
+	}
+	
+	public String getClassName()
+	{
+		return className;
+	}
+	
+	public MyDate 
+}
