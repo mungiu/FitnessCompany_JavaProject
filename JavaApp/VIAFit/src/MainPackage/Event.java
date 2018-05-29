cd0346003a15818044d88ea9d8a0997edcc215c2
Andrei Mungiu <andrei_mungiu@yahoo.com>
2018-05-29 11:42:50 +0200

    event code up

--- JavaApp/VIAFit/src/MainPackage/Event.java
+++ JavaApp/VIAFit/src/MainPackage/Event.java
@@ -5,6 +5,10 @@ public class Event
 	private String className;
 	private boolean requiresQualification;
 	private int maxMembers;
+	private MyDate startDate;
+	private MyDate endDate;
+	private MyClock startTime;
+	private MyClock endTime;
 
 	public Event(String className, MyDate startDate, MyDate endDate, MyClock startTIme, MyClock endTime)
 	{
@@ -16,5 +20,4 @@ public class Event
 		return className;
 	}
 	
-	public MyDate 
 }
