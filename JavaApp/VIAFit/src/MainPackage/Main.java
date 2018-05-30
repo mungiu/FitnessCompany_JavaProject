0a255cb75d1f0d219928f033cf42c81f42342d7c
Da wae <eric.volmer@hotmail.com>
2018-05-30 14:30:04 +0200

    Update

--- JavaApp/VIAFit/src/MainPackage/Main.java
+++ JavaApp/VIAFit/src/MainPackage/Main.java
@@ -2,31 +2,42 @@ package MainPackage;
 
 import java.util.ArrayList;
 
-public class Main {
-   
-   
-   public boolean IAvailable()
+public class Main
+{
+
+   private ArrayList<Event> onGoingEventsList, upComingEventsList,
+         allEventsList;
+   private ArrayList<ClassType> allClassesList;
+   private ArrayList<Member> memberList;
+   private ArrayList<Instructor> instructorList;
+
+   public ArrayList<Event> getOnGoingEventList()
    {
-      
+      return onGoingEventsList;
    }
-   
-   
-   
-	public static void main(String[] args) {
-   
-}
-	private ArrayList<Event> onGoingEventsList, upComingEventsList,
-	allEventsList;
-	private ArrayList<ClassType> allClassesList;
-	private ArrayList<Member> memberList;
-	private ArrayList<Instructor> instructorList;
-	
 
-	public ArrayList<Event> getOnGoingEventList() {
-		return onGoingEventsList;
-	}
+   public ArrayList<Event> getUpComingEventsList()
+   {
+      return upComingEventsList;
+   }
 
-	public ArrayList<Event> getUpComingEventsList() {
-		return upComingEventsList;
-	}
+   public boolean IsAvailable(Instructor instructor, Event event)
+   {
+      for (int i = 0; i < allEventsList.size(); i++)
+         if (allEventsList.get(i).getInstructorsList().contains(instructor))
+            for (int j = 0; j < allEventsList.size(); j++)
+               if (allEventsList.get(i).getStarTime()
+                     .isBefore(event.getStarTime())
+                     && (event.getStarTime().isBefore(
+                           allEventsList.get(i).getEndTime()) == false))
+                  return true;
+
+      return false;
+   }
+
+   public static void main(String[] args)
+   {
+
+   }
+
 }
