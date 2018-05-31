310789720e7913b8894fb6a2984f568401c14144
Andrei Mungiu <andrei_mungiu@yahoo.com>
2018-05-31 11:36:05 +0200

    code UP

--- JavaApp/VIAFit/src/MainPackage/ClassType.java
+++ JavaApp/VIAFit/src/MainPackage/ClassType.java
@@ -2,6 +2,33 @@ package MainPackage;
 
 public class ClassType
 {
+	private String className;
 
-   private String name;
+	public ClassType(String className)
+	{
+		this.className = className;
+	}
+
+	public String getClassName()
+	{
+		return className;
+	}
+
+	public void setClassName(String className)
+	{
+		this.className = className;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (!(obj instanceof ClassType))
+			return false;
+		else
+		{
+			ClassType temp = (ClassType) obj;
+
+			return className == temp.className;
+		}
+	}
 }
--- JavaApp/VIAFit/src/MainPackage/Instructor.java
+++ JavaApp/VIAFit/src/MainPackage/Instructor.java
@@ -6,65 +6,72 @@ import org.w3c.dom.events.Event;
 
 public class Instructor
 {
-   private int instructorID;
-   private String firstName, lastName;
-   private boolean isQualified;
-   private ArrayList<ClassType> qualifiedClassList;
-   private ArrayList<Main> allTaughtEventsList;
+	private int instructorID = 1;
+	private String firstName, lastName;
+	private boolean isQualified;
+	private ArrayList<ClassType> qualifiedClassesList;
+	private ArrayList<Main> allTaughtEventsList;
 
-   public Instructor(int instructorID, String firstName, String lastName,
-         boolean isQualified)
-   {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.instructorID = instructorID;
-      this.isQualified = isQualified;
-      allTaughtEventsList = new ArrayList<Main>();
-      qualifiedClassList = new ArrayList<ClassType>();
-   }
+	public Instructor(String firstName, String lastName, boolean isQualified)
+	{
+		// TODO: pull new instructorID from binary file by checking biggest instructor ID
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.isQualified = isQualified;
+		allTaughtEventsList = new ArrayList<Main>();
+		qualifiedClassesList = new ArrayList<ClassType>();
+	}
 
-   public String getFirstName()
-   {
-      return firstName;
-   }
+	public String getFirstName()
+	{
+		return firstName;
+	}
 
-   public void setFirstName(String firstName)
-   {
-      this.firstName = firstName;
-   }
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
 
-   public String getLastName()
-   {
-      return lastName;
-   }
+	public String getLastName()
+	{
+		return lastName;
+	}
 
-   public void setLastName(String lastName)
-   {
-      this.lastName = lastName;
-   }
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
 
-   public int getInstructorID()
-   {
-      return instructorID;
-   }
-   
-   public boolean getIsQualified()
-   {
-      return isQualified;
-   }
+	public int getInstructorID()
+	{
+		return instructorID;
+	}
 
-   public void setIsQualified(boolean isQualified)
-   {
-      this.isQualified = isQualified;
-   }
-   public ArrayList<ClassType> getQualifiedClassesList()
-   {
-      return qualifiedClassList;
-   }
-   public ArrayList<Main> getAllTaughtEvents()
-   {
-      return allTaughtEventsList;
-   }
-   
+	public boolean getIsQualified()
+	{
+		return isQualified;
+	}
+
+	public void setIsQualified(boolean isQualified)
+	{
+		this.isQualified = isQualified;
+	}
+
+	public ArrayList<ClassType> getQualifiedClassesList()
+	{
+		return qualifiedClassList;
+	}
+
+	public ArrayList<Main> getAllTaughtEvents()
+	{
+		return allTaughtEventsList;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Instructor [instructorID=" + instructorID + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", isQualified=" + isQualified + "]";
+	}
 
 }
