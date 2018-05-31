39c7991589c6013d1e44867c560eeee2f473d18f
Andrei Mungiu <andrei_mungiu@yahoo.com>
2018-05-31 12:07:45 +0200

    code UP

--- JavaApp/VIAFit/src/MainPackage/Main.java
+++ JavaApp/VIAFit/src/MainPackage/Main.java
@@ -5,49 +5,55 @@ import java.util.ArrayList;
 public class Main
 {
 
-   private ArrayList<Event> onGoingEventsList, upComingEventsList,
-         allEventsList;
-   private ArrayList<ClassType> allClassesList;
-   private ArrayList<Member> memberList;
-   private ArrayList<Instructor> instructorList;
+	private ArrayList<Event> onGoingEventsList, upComingEventsList, allEventsList;
+	private ArrayList<ClassType> allClassTypeList;
+	private ArrayList<Member> memberList;
+	private ArrayList<Instructor> instructorList;
 
-   public ArrayList<Event> getOnGoingEventList()
-   {
-      return onGoingEventsList;
-   }
+	public ArrayList<Event> getOnGoingEventList()
+	{
+		return onGoingEventsList;
+	}
 
-   public ArrayList<Event> getUpComingEventsList()
-   {
-      return upComingEventsList;
-   }
+	public ArrayList<Event> getUpComingEventsList()
+	{
+		return upComingEventsList;
+	}
 
-   public boolean getInstructorIsAvailable(Instructor instructor, Event event)
-   {
-      for (int i = 0; i < allEventsList.size(); i++)
-         if (allEventsList.get(i).getInstructorsList().contains(instructor))
-            for (int j = 0; j < allEventsList.size(); j++)
-               if (allEventsList.get(i).getStarTime()
-                     .isBefore(event.getStarTime())
-                     && (event.getStarTime().isBefore(
-                           allEventsList.get(i).getEndTime()) == false))
-                  return true;
+	public boolean getInstructorIsAvailable(Instructor instructor, Event event)
+	{
+		for (int i = 0; i < allEventsList.size(); i++)
+			if (allEventsList.get(i).getInstructorsList().contains(instructor))
+				for (int j = 0; j < allEventsList.size(); j++)
+					if (allEventsList.get(i).getStarTime().isBefore(event.getStarTime())
+							&& (event.getStarTime().isBefore(allEventsList.get(i).getEndTime()) == false))
+						return true;
 
-      return false;
-   }
+		return false;
+	}
 
-   public ArrayList<ClassType> getAllClassesList()
-   {
-      return allClassesList;
-   }
+	public ArrayList<ClassType> getAllClassTypeList()
+	{
+		return allClassTypeList;
+	}
 
-   public ArrayList<Member> getMemberList()
-   {
-      return memberList;
-   }
+	public ArrayList<Member> getMemberList()
+	{
+		return memberList;
+	}
 
-   public ArrayList<Instructor> getInstructorList()
-   {
-      return instructorList;
-   }
+	public ArrayList<Instructor> getInstructorList()
+	{
+		return instructorList;
+	}
 
+	public void updateOnGoingEventsList()
+	{
+		// TODO finalize
+	}
+	
+	public void updateUpComingEventsList()
+	{
+		// TODO finalize
+	}
 }
