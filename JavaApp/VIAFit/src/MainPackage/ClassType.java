7960e3e46467f6480a08b9a15660c8f1efa60d2e
Da wae <eric.volmer@hotmail.com>
2018-06-06 13:31:45 +0200

    ClassType commented

--- JavaApp/VIAFit/src/MainPackage/ClassType.java
+++ JavaApp/VIAFit/src/MainPackage/ClassType.java
@@ -2,39 +2,74 @@ package MainPackage;
 
 import java.io.Serializable;
 
+/**
+ * A class representing ClassType with the class name.
+ * 
+ * @author Group 1
+ */
 public class ClassType implements Serializable
 {
-	private static final long serialVersionUID = -7574607274053354338L;
-	private String className;
+   private static final long serialVersionUID = -7574607274053354338L;
+   private String className;
 
-	public ClassType(String className)
-	{
-		this.className = className;
-	}
+   /**
+    * One-argument constructor.
+    * 
+    * @param className
+    *           is the ClassType classes name.
+    */
+   public ClassType(String className)
+   {
+      this.className = className;
+   }
 
-	public String getClassName()
-	{
-		return className;
-	}
+   /**
+    * Gets the ClassTypes class name.
+    * 
+    * @return the ClassTypes class name.
+    */
+   public String getClassName()
+   {
+      return className;
+   }
 
-	public void setClassName(String className)
-	{
-		this.className = className;
-	}
+   /**
+    * Sets the ClassTypes class name.
+    * 
+    * @param className
+    *           is what the ClassTypes class name will be set to.
+    */
+   public void setClassName(String className)
+   {
+      this.className = className;
+   }
 
-	public boolean equals(Object obj)
-	{
-		if (!(obj instanceof ClassType))
-		   {
-		      return false;
-		   }
-			ClassType temp = (ClassType) obj;
+   /**
+    * Compares class name of two ClassTypes
+    * 
+    * @param obj
+    *           the object to compare with
+    * @return true if the given object is equal to this ClassType.
+    */
+   public boolean equals(Object obj)
+   {
+      if (!(obj instanceof ClassType))
+      {
+         return false;
+      }
+      ClassType temp = (ClassType) obj;
 
-			return className.equals(temp.className);
-	}
+      return className.equals(temp.className);
+   }
 
-	public String toString()
-	{
-		return className;
-	}
+   /**
+    * Returns a string representation of the ClassType.
+    * 
+    * @return a string representation of the ClassType in the format of
+    *         "className".
+    */
+   public String toString()
+   {
+      return className;
+   }
 }
